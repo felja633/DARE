db0a21bd84f08ea8f1027a2df14a8ced69deffbe
felix <felix.jaremo-lawin@liu.se>
2018-09-06 22:14:12 +0200

    fixed bugs in fps and plotting

--- src/point_cloud_plotting.py
+++ src/point_cloud_plotting.py
@@ -9,11 +9,11 @@ def plotClouds(Vs, colors, fignum):
     ax = fig.add_subplot(111, projection='3d')
     if colors: 
         for V, color in zip(Vs, colors):
-            ax.scatter(V[0,:], V[1,:], V[2,:], c=color, marker='o', s=1)
+            ax.scatter(V[0,:], V[1,:], V[2,:], c=color.transpose()/255.0, marker='o', s=9)
     else:
         for i, V in enumerate(Vs):
             cind = min(6,i)
-            ax.scatter(V[0,:], V[1,:], V[2,:], c=color_list[cind], marker='o', s=1)
+            ax.scatter(V[0,:], V[1,:], V[2,:], c=color_list[cind], marker='o', s=9)
 
 
     ax.set_xlabel('X')
@@ -21,17 +21,24 @@ def plotClouds(Vs, colors, fignum):
     ax.set_zlabel('Z')
     plt.show()
 
-def plotCloudsModel(Vs, X, fignum):
+def plotCloudsModel(Vs, colors, X, fignum):
     color_list = ['r','g','b','c','y','m']    
     fig = plt.figure(fignum)
     ax = fig.add_subplot(111, projection='3d')
 
+    if colors: 
+        for V, color in zip(Vs, colors):
+            Vc = rs.random_sampling([V,color], 2000)
+            V = Vc[0]
+            cc = Vc[1].transpose()
+            ax.scatter(V[0,:], V[1,:], V[2,:], c=cc/255.0, marker='o', s=9)
+
     for i, V in enumerate(Vs):
         Vv = rs.random_sampling(V, 2000) 
         cind = min(5,i)
-        ax.scatter(Vv[0,:], Vv[1,:], Vv[2,:], c=color_list[cind], marker='o', s=4)
+        ax.scatter(Vv[0,:], Vv[1,:], Vv[2,:], c=color_list[cind], marker='o', s=9)
 
-    ax.scatter(X[0,:], X[1,:], X[2,:], c='k', marker='o', s=16)
+    ax.scatter(X[0,:], X[1,:], X[2,:], c='k', marker='o', s=80)
 
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
--- src/resampling.py
+++ src/resampling.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 def random_sampling(pts, num_points_left):
-
+    
     def rnd_samp(nump, s):
 
         arr = np.arange(0, nump)
@@ -11,7 +11,6 @@ def random_sampling(pts, num_points_left):
 
         arr = np.random.permutation(arr)
         return arr[0:s]
-
 
     if isinstance(pts, (list,)):
         sh = pts[0].shape
@@ -22,15 +21,24 @@ def random_sampling(pts, num_points_left):
         arr = rnd_samp(sh[1], num_points_left)
         return pts[:,arr]
 
+
 def farthest_point_sampling(pts, num_points_left):
     """
     Select a subset of K points from pts using farthest point sampling
     from: https://codereview.stackexchange.com/questions/179561/farthest-point-algorithm-in-python
     """
-    farthest_pts = np.zeros((num_points_left, pts.shape[1]))
-    farthest_pts[0] = pts[np.random.randint(len(pts))]
-    distances = calc_distances(farthest_pts[0], pts)
+    def calc_distances(p0, points):
+        return ((p0[:, np.newaxis] - points)**2).sum(axis=0)
+
+    farthest_pts = np.zeros((pts.shape[0], num_points_left))
+    f_ind = np.random.randint(pts.shape[1])
+    farthest_pts[:,0] = pts[:,f_ind]
+    distances = calc_distances(farthest_pts[:,0], pts)
+    inds = []
+    inds.append(f_ind)
     for i in range(1, num_points_left):
-        farthest_pts[i] = pts[np.argmax(distances)]
-        distances = np.minimum(distances, calc_distances(farthest_pts[i], pts))
-    return farthest_pts
+        farthest_pts[:,i] = pts[:,np.argmax(distances)]
+        inds.append(np.argmax(distances))
+        distances = np.minimum(distances, calc_distances(farthest_pts[:,i], pts))
+
+    return farthest_pts, inds
